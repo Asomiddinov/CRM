b21f61429970e8caca49264ed16a06a620675996
Bobir Asomiddinov <basomiddinov@gmail.com>
2023-01-08 17:06:31 +0500

    Added some Bootstrap to forms

--- application/forms.py
+++ application/forms.py
@@ -1,4 +1,4 @@
-from wtforms import StringField, SubmitField, IntegerField, BooleanField
+from wtforms import StringField, SubmitField, IntegerField, RadioField
 from wtforms.fields import DateField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length
@@ -13,11 +13,14 @@ class QRCodeData(FlaskForm):
 class Mine(FlaskForm):
     client = StringField("Client", validators=[
                          DataRequired(), Length(min=3, max=500)])
+    address = StringField("Address", validators=[
+                          DataRequired(), Length(min=5, max=500)])
     quantity = StringField("Quantity", validators=[
                            DataRequired(), Length(min=3, max=500)])
     mark = StringField("Mark", validators=[
                        DataRequired(), Length(min=3, max=100)])
     price = IntegerField("Price", validators=[DataRequired()])
-    paid = BooleanField("Is Paid?", validators=[DataRequired()])
-    date = DateField("Date")
+    paid = RadioField("Status", choices=[
+                      "Paid", "Unpaid"], validators=[DataRequired()])
+    date = DateField("Date", validators=[DataRequired()])
     submit = SubmitField("Confirm")

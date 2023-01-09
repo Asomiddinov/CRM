0f677224242f227bb84b825e143d592c5901eb8d
Bobir Asomiddinov <basomiddinov@gmail.com>
2023-01-09 10:07:49 +0500

    installed autopep8, added some forms

--- application/forms.py
+++ application/forms.py
@@ -1,4 +1,4 @@
-from wtforms import StringField, SubmitField, IntegerField, RadioField
+from wtforms import StringField, SubmitField, IntegerField, RadioField, SelectField
 from wtforms.fields import DateField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length
@@ -20,7 +20,10 @@ class Mine(FlaskForm):
     mark = StringField("Mark", validators=[
                        DataRequired(), Length(min=3, max=100)])
     price = IntegerField("Price", validators=[DataRequired()])
+    currency = SelectField("Currency", choices=["UZS", "USD", "EUR", "RUB"])
     paid = RadioField("Status", choices=[
                       "Paid", "Unpaid"], validators=[DataRequired()])
+    driver = StringField("Driver", validators=[
+                         DataRequired(), Length(min=5, max=500)])
     date = DateField("Date", validators=[DataRequired()])
     submit = SubmitField("Confirm")
